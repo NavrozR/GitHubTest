32411aad9b12087de2e07ee06cc9f896d584d0d9
Navroz <navrozrashyani@gmail.com>
2020-05-30 09:29:50 +0100

    first commit

---
+++ GitHubTest/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubTest
+{
+    public class Class1
+    {
+        static void Main(string[] args)
+        {
+        
+        Console.WriteLine("Hello India");
+                    
+        }
+              
+    }
+}
